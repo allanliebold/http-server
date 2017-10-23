3fdea7854f29b610773d2c893201149958d309bf
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-23 15:31:29 -0700

    fixing linter errors on test file

--- test_servers.py
+++ test_servers.py
@@ -1,9 +1,19 @@
+"""Setup test functions.
+
+To verify proper operation of server and client sockets.
+
+"""
+
+
 import sys
 import socket
 
+
 def test_server():
+    """Test the server socket setup."""
     assert True
+
 
 def test_client():
+    """Test the client socket setup."""
     assert True
-
