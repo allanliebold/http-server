d7d1ac93efcb1949dccdf69ffdfb136b827edd97
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-27 17:10:16 -0700

    added test for improperly formatted request

--- src/test_servers.py
+++ src/test_servers.py
@@ -115,7 +115,7 @@ def test_client_sending_valid_request():
     from client import client
     from server import response_ok
     req = 'GET /path/to/index.html HTTP/1.1\r\nHost: www.mysite1.com:80\r\n\r\n'
-    assert client(req) == "HTTP/1.1 200\nOK\r\n"
+    assert client(req) == response_ok().decode('utf-8')
 
 
 def test_client_sending_request_with_wrong_method():
@@ -123,3 +123,11 @@ def test_client_sending_request_with_wrong_method():
     from client import client
     req = 'PUT /path/to/index.html HTTP/1.1\r\nHost: www.mysite1.com:80\r\n\r\n'
     assert client(req) == "HTTP/1.1 405 Method Not Allowed"
+
+
+def test_client_sending_request_with_wrong_formatting():
+    """Test that when client sends wrongly formatted request, gets 400 error."""
+    from client import client
+    from server import parse_request
+    req = 'GET /path/to/index.html HTTP/1.1\r\nHost: www.mysite1.com:80\r\n'
+    assert client(req) == "HTTP/1.1 400 Improperly Formed Request"
