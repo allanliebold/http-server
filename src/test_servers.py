0a8a2277f8fa6fa86632f964c6d7ddb299a3389f
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-19 16:45:14 -0800

    fixed tests so that they pass for both py27 and py36.

--- src/test_servers.py
+++ src/test_servers.py
@@ -9,15 +9,13 @@ from __future__ import unicode_literals
 def test_client():
     """Test the client socket setup."""
     from client import client
-    from server import response_ok
-    assert client("Send this") == response_ok()
+    assert client("Send this") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_shorter_than_one_buffer():
     """Test if message is shorter than buffer, still works."""
     from client import client
-    from server import response_ok
-    assert client('hi') == response_ok()
+    assert client('hi') == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_longer_than_one_buffer_length():
@@ -25,54 +23,46 @@ def test_messages_longer_than_one_buffer_length():
     long_message = "This is a super long message that is way longer than \
         the default buffer length"
     from client import client
-    from server import response_ok
-    assert client(long_message) == response_ok()
+    assert client(long_message) == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_exact_multiplier_of_buffer():
     """Test messages that are exact buffer size multiples work."""
     from client import client
-    from server import response_ok
-    assert client("12345678") == response_ok()
+    assert client("12345678") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_1():
     """Test that when sending "1" we get back a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client("1") == response_ok()
+    assert client("1") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_2():
     """Test that when sending "12" we get back a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client("12") == response_ok()
+    assert client("12") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_3():
     """Test that when sending "123" we get back a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client("123") == response_ok()
+    assert client("123") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_6():
     """Test that when sending "123456" we get back a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client("123456") == response_ok()
+    assert client("123456") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_7():
     """Test that when sending "1234567" we get back a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client("1234567") == response_ok()
+    assert client("1234567") == "HTTP/1.1 200\nOK\r\n"
 
 
 def test_messages_with_non_ascii_chars():
     """Test that non ascii chars are sent and get a 200 OK response."""
     from client import client
-    from server import response_ok
-    assert client(u"Œš™- word yo!") == response_ok()
+    assert client(u"Œš™- word yo!") == "HTTP/1.1 200\nOK\r\n"
